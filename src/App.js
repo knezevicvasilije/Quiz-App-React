443b3f9b263942fb45f0b406a4459b855dd9ef5a
knezevicvasilije <73421159+knezevicvasilije@users.noreply.github.com>
2021-01-27 20:56:12 +0100

    Questions added on mount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import "./assets/style.css";
+import quizService from "./quizService";
 
 export default class QuizBee extends React.Component {
+  state = {
+    questionBank: []
+  };
+  getQuestions = () => {
+    quizService().then((question) => {
+      this.setState({
+        questionBank: question
+      });
+    });
+  };
+  componentDidMount() {
+    this.getQuestions();
+  }
+
   render() {
     return (
       <div className="container">
         <div className="title">DevLab Quiz</div>
+        {this.state.questionBank.length > 0 &&
+          this.state.questionBank.map(
+            ({ question, answers, correct, questionId }) => <h4>{question}</h4>
+          )}
       </div>
     );
   }
